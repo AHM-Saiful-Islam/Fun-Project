52808ff2df10ff08db3eaa97c7c69a52484d8f90
A H M SAIFUL ISLAM <iahmsaiful@gmail.com>
2024-12-04 15:25:12 +0100

    Add date options and related functionality

--- Week-Planer/weekplanner/planner/forms.py
+++ Week-Planer/weekplanner/planner/forms.py
@@ -4,7 +4,8 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description', 'day', 'time']
+        fields = ['title', 'description', 'day', 'time', 'date']
         widgets = {
             'time': forms.TimeInput(format='%H:%M', attrs={'type': 'time'}),
+            'date': forms.DateInput(format='%Y-%m-%d', attrs={'type': 'date'}),
         }
--- Week-Planer/weekplanner/planner/models.py
+++ Week-Planer/weekplanner/planner/models.py
@@ -16,8 +16,9 @@ class Task(models.Model):
     description = models.TextField(blank=True)
     day = models.CharField(max_length=10, choices=DAYS_OF_WEEK)
     time = models.TimeField()
+    date = models.DateField()
     completed = models.BooleanField(default=False)
 
     def __str__(self):
         # return f"{self.title} - {self.day}"
-        return f"{self.title} - {self.day} {'[Completed]' if self.completed else ''}"
+        return f"{self.title} - {self.date} - {self.day} {'[Completed]' if self.completed else ''}"
